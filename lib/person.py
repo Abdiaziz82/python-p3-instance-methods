f890e6315092d3c5ae7ab9f5224f08ca2b039a7d
Abdiaziz82 <abdiazizhared64@gmail.com>
2024-08-16 22:30:06 +0300

    initial commit

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
-
 class Person:
     # Class body goes here
+  def talk(self):
+     print("Hello World!")
+  
+  def  walk(self):
+    print ("The person is walking.")
 
-    #Instance method definition
-    pass
+Abdi = Person()
+Abdi.talk()
+Abdi.walk()
+# class Person:
+#     def talk(self):
+#         print("Hello World!")
+
+#     def walk(self):
+#         print("The person is walking.")
+
+# Abdi = Person()
+# Abdi.talk()
+# Abdi.walk()
